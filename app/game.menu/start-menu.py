b8c3902719dd4d24693d754afcf54961ac1b8354
balljul <juliusball197@gmail.com>
2023-10-09 15:18:53 +0200

    ADDED all the buttons into the menu

--- app/game.menu/start-menu.py
+++ app/game.menu/start-menu.py
@@ -18,16 +18,20 @@ font = pygame.font.SysFont("arialBlack", 40)
 TEXT_COL = (255, 255, 255)
 
 # loading btn images
-resume_img = pygame.image.load("./Fortsetzen.png").convert_alpha()
+resume_img = pygame.image.load("./resume.png").convert_alpha()
 quit_img = pygame.image.load("./quit.png").convert_alpha()
+options_img = pygame.image.load("./options.png").convert_alpha()
+
 # Creating Button instances
 resume_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), resume_img, 0.2)
 quit_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/2), quit_img, 0.2)
+options_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/1.4), options_img, 0.2)
 
 # Functions
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+
 
 # Game Loop Variables
 run = True
@@ -43,7 +47,9 @@ while run:
         if resume_btn.draw(screen):
             game_paused = False
         if quit_btn.draw(screen):
-            run = False            
+            run = False
+        if options_btn.draw(screen):
+            pass
     else:
         draw_text("Press space for menu", font, TEXT_COL, (SCREEN_WIDTH/2.9), (SCREEN_HEIGHT/2))
 
