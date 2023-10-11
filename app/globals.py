ed6153503927011369f1d63b26d166e4efca6c24
balljul <juliusball197@gmail.com>
2023-10-11 11:13:44 +0200

    UPDATED globals

--- app/globals.py
+++ app/globals.py
@@ -3,15 +3,14 @@ pygame.init()
 
 
 class AFX_configs():
-    def __init__(self):
-        self.run = True
-        self.SCREEN_WIDTH = 1920
-        self.SCREEN_HEIGHT = 1080
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.dt = 0
-        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
-        self.font = pygame.font.SysFont("arialBlack", 40)
-        self.TEXT_COL = (255, 255, 255)
-        self.keys = pygame.key.get_pressed()
+    running = True
+    SCREEN_WIDTH = 1920
+    SCREEN_HEIGHT = 1080
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    font = pygame.font.SysFont("arialBlack", 40)
+    TEXT_COL = (255, 255, 255)
+    keys = pygame.key.get_pressed()
