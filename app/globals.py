1664854f0e333b79895a0d4632c8c58d4faae970
balljul <juliusball197@gmail.com>
2023-10-11 09:18:59 +0200

    ADDED the config class

--- app/globals.py
+++ app/globals.py
@@ -1,3 +1,14 @@
-class configs():
+import pygame
+pygame.init()
+
+
+class AFX_configs():
     def __init__(self):
-        self.run = True
+        self.run = Trued
+        self.SCREEN_WIDTH = 1920
+        self.SCREEN_HEIGHT = 1080
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.dt = 0
+        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
