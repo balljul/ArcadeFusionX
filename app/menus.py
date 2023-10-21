586171166b8d4591a27df8efc72a94009c499510
balljul <juliusball197@gmail.com>
2023-10-21 13:40:10 +0200

    ADDED the start menu

--- app/menus.py
+++ app/menus.py
@@ -3,6 +3,21 @@ import button
 from globals import AFX_configs as configs
 
 
+class Start_menu():
+
+    # Defining constants for the menu
+    screen = configs.screen
+    SCREEN_WIDTH = configs.SCREEN_WIDTH
+    SCREEN_HEIGHT = configs.SCREEN_HEIGHT
+    font = configs.font
+    TEXT_COL = configs.TEXT_COL
+    game_paused = False
+    # loading btn images
+    start_img = pygame.image.load("resources/start.png").convert_alpha()
+    # Creating btn Instances
+    start_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), start_img, 0.5)
+
+
 class Game_menu():
     # Defining constants for the menu
     screen = configs.screen
@@ -14,18 +29,16 @@ class Game_menu():
     # loading btn images
     resume_img = pygame.image.load("resources/resume.png").convert_alpha()
     quit_img = pygame.image.load("resources/quit.png").convert_alpha()
-    options_img = pygame.image.load("resources/options.png").convert_alpha()
+    # options_img = pygame.image.load("resources/options.png").convert_alpha()
 
     # Creating Button instances
     resume_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), resume_img, 0.2)
     quit_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/2), quit_img, 0.2)
-    options_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/1.4), options_img, 0.2)
-
+    # options_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/1.4), options_img, 0.2)
 
     def draw_text(self, text, x, y):
         img = self.font.render(text, True, self.TEXT_COL)
         self.screen.blit(img, (x, y))
-
 
     def game_menu_logic(self):
 
@@ -43,3 +56,4 @@ class Game_menu():
                 pass
             else:
                 self.draw_text("Press space for menu", self.font, self.TEXT_COL, (self.SCREENW/2.9), (self.SCREEN_HEIGHT/2))
+
