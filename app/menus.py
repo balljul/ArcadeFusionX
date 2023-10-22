9217f0b85cc6ed7003fd5c5ce1ef350099d1df03
balljul <juliusball197@gmail.com>
2023-10-22 18:26:45 +0200

    MERGED menu classes into one class

--- app/menus.py
+++ app/menus.py
@@ -3,22 +3,7 @@ import button
 from globals import AFX_configs as configs
 
 
-class Start_menu():
-
-    # Defining constants for the menu
-    screen = configs.screen
-    SCREEN_WIDTH = configs.SCREEN_WIDTH
-    SCREEN_HEIGHT = configs.SCREEN_HEIGHT
-    font = configs.font
-    TEXT_COL = configs.TEXT_COL
-    game_paused = False
-    # loading btn images
-    start_img = pygame.image.load("../resources/start.png").convert_alpha()
-    # Creating btn Instances
-    start_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), start_img, 0.5)
-
-
-class Game_menu():
+class AFX_menus():
     # Defining constants for the menu
     screen = configs.screen
     SCREEN_WIDTH = configs.SCREEN_WIDTH
@@ -29,31 +14,11 @@ class Game_menu():
     # loading btn images
     resume_img = pygame.image.load("../resources/resume.png").convert_alpha()
     quit_img = pygame.image.load("../resources/quit.png").convert_alpha()
-    # options_img = pygame.image.load("resources/options.png").convert_alpha()
+    start_img = pygame.image.load("../resources/start.png").convert_alpha()
+    pause_img = pygame.image.load("../resources/pause_btn.png")
 
     # Creating Button instances
     resume_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), resume_img, 0.2)
     quit_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/2), quit_img, 0.2)
-    # options_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/1.4), options_img, 0.2)
-
-    def draw_text(self, text, x, y):
-        img = self.font.render(text, True, self.TEXT_COL)
-        self.screen.blit(img, (x, y))
-
-    def game_menu_logic(self):
-
-        keys = configs.keys
-        if keys[pygame.K_p]:
-            self.game_paused = True
-
-        # Check if Game is paused
-        if self.game_paused:
-            if self.resume_btn.draw(self.screen):
-                self.game_paused = False
-            if self.quit_btn.draw(self.screen):
-                configs.running = False
-            if self.options_btn.draw(self.screen):
-                pass
-            else:
-                self.draw_text("Press space for menu", self.font, self.TEXT_COL, (self.SCREENW/2.9), (self.SCREEN_HEIGHT/2))
-
+    start_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), start_img, 0.5)
+    pause_btn = button.Button((SCREEN_WIDTH/2.2), (SCREEN_HEIGHT/4), pause_img, 0.5)
